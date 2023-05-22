995210ba901b430b987643bfc728c7b231b0839e
Rafael Lee <rafaellee.img@gmail.com>
2023-05-22 12:26:44 +0800

    Update USB driver's name

--- usbhid.c
+++ usbhid.c
@@ -293,7 +293,9 @@ int main(void)
 		__asm__("nop");
 	}
 
-	usbd_dev = usbd_init(&st_usbfs_v1_usb_driver, &dev_descr, &config, usb_strings, 3, usbd_control_buffer, sizeof(usbd_control_buffer));
+	usbd_dev = usbd_init(&otghs_usb_driver, &dev_descr, &config,
+		usb_strings, 3,
+		usbd_control_buffer, sizeof(usbd_control_buffer));
 	usbd_register_set_config_callback(usbd_dev, hid_set_config);
 
 	while (1)
